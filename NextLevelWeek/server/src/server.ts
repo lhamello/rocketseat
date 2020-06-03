a0fc418ffd2b74fda4062179d56814844192b777
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-06-03 01:50:51 -0300

    criado rota para buscar apenas um usuário

--- NextLevelWeek/server/src/server.ts
+++ NextLevelWeek/server/src/server.ts
@@ -2,15 +2,24 @@ import express, { request, response } from 'express';
 
 const app = express();
 
+const users = [
+    'Luiz',
+    'Rosi',
+    'Cecilia',
+    '?'
+];
+
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
-    
-    response.json([
-        'Luiz',
-        'Rosi',
-        'Cecilia',
-        '?'
-    ]);
+
+    response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    response.json(user);
 });
 
 app.post('/users', (request, response) => {
