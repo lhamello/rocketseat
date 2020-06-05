ee479aa392807c6df02a080c0b2ff134620e3d64
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-06-05 01:33:33 -0300

    criado controllers para agrupamento de rotas por recurso

---
+++ NextLevelWeek/server/src/controllers/PointsController.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+
+class PointsController {
+
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            items
+        } = request.body;
+
+        const trx = await Knex.transaction();
+
+        const insertedIds = await trx('points').insert({
+            image: 'image-fake',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf
+        })
+
+        const point_id = insertedIds[0];
+
+        const pointItems = items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id,
+            }
+        });
+
+        await trx('point_items').insert(pointItems);
+
+        return response.json({ sucess: true })
+        );
+    }
+}
+
+export default PointsController;
--- NextLevelWeek/server/src/routes.ts
+++ NextLevelWeek/server/src/routes.ts
@@ -1,7 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
 import Knex from './database/connection';
+import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+const PointsController = new PointsController();
 
 routes.get('/items', async (request, response) => {
     const items = await Knex('items').select('*');
@@ -15,45 +17,6 @@ routes.get('/items', async (request, response) => {
     });
 
     return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    const trx = await Knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    })
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    });
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({ sucess: true })
 });
 
 export default routes;
