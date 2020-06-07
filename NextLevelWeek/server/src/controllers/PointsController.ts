c66840ba2d336f27d2cf09ff99aca16f68aaee96
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-06-07 01:55:42 -0300

    adicionado cors e adicionada imagem padrão ao inserir ponto de coleta

--- NextLevelWeek/server/src/controllers/PointsController.ts
+++ NextLevelWeek/server/src/controllers/PointsController.ts
@@ -52,7 +52,7 @@ class PointsController {
         const trx = await Knex.transaction();
 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60s',
             name,
             email,
             whatsapp,
