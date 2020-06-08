2673098aab16d668e508392778aed4ce8315dc06
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-06-07 23:14:58 -0300

    funcionalidade de marcação de pontos no mapa

--- NextLevelWeek/web/src/pages/CreatePoint/index.tsx
+++ NextLevelWeek/web/src/pages/CreatePoint/index.tsx
@@ -8,6 +8,7 @@ import logo from '../../assets/logo.svg';
 
 import './styles.css';
 import axios from 'axios';
+import { LeafletMouseEvent } from 'leaflet';
 
 interface Item {
     id: number;
@@ -30,6 +31,7 @@ const CreatePoint = () => {
 
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -65,6 +67,13 @@ const CreatePoint = () => {
     function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
         const city = event.target.value;
         setSelectedCity(city);
+    }
+
+    function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPosition([
+            event.latlng.lat,
+            event.latlng.lng,
+        ]);
     }
 
     return (
@@ -110,12 +119,12 @@ const CreatePoint = () => {
                         <span>Selecione o Enderço no mapa</span>
                     </legend>
 
-                    <Map center={[-27.2092052, -49.6401092]} zoom={15}>
+                    <Map center={[-27.2092052, -49.6401092]} zoom={15} onclick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-                        <Marker position={[-27.2092052, -49.6401092]} />
+                        <Marker position={selectedPosition} />
                     </Map>
 
                     <div className="field-group">
