50f3f6b029448456390dc0c71cf451005cbf5a6e
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-06-23 02:08:14 -0300

    adicionado filtro de UF e cidade na tela principal

--- NextLevelWeek/mobile/src/pages/Home/index.tsx
+++ NextLevelWeek/mobile/src/pages/Home/index.tsx
@@ -1,40 +1,54 @@
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { Image, ImageBackground, KeyboardAvoidingView, StyleSheet, Text, View, Platform } from 'react-native';
+import { RectButton, TextInput } from 'react-native-gesture-handler';
 
 const Home = () => {
+    const [uf, setUf] = useState('');
+    const [city, setCity] = useState('');
+
     const navigation = useNavigation();
 
     function handleNavigationToPoints() {
-        navigation.navigate('Points');
+        navigation.navigate('Points', {
+            uf,
+            city
+        });
     }
 
     return (
-        <ImageBackground
-            style={styles.container}
-            source={require('../../assets/home-background.png')}
-            imageStyle={{ width: 274, height: 368 }}>
-            <View style={styles.main}>
-                <Image source={require('../../assets/logo.png')} />
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-            </View>
-
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={handleNavigationToPoints}>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="arrow-right" color="FFF" size={24} />
-                        </Text>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+            <ImageBackground
+                style={styles.container}
+                source={require('../../assets/home-background.png')}
+                imageStyle={{ width: 274, height: 368 }}>
+                <View style={styles.main}>
+                    <Image source={require('../../assets/logo.png')} />
+                    <View>
+                        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
                     </View>
-                    <Text style={styles.buttonText}>
-                        Entrar
+                </View>
+
+                <View style={styles.footer}>
+                    <TextInput style={styles.input} placeholder="Digite a UF" value={uf} onChangeText={setUf} maxLength={2} autoCapitalize="characters" autoCorrect={false} />
+
+                    <TextInput style={styles.input} placeholder="Digite a Cidade" value={city} onChangeText={setCity} autoCorrect={false} />
+
+                    <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+                        <View style={styles.buttonIcon}>
+                            <Text>
+                                <Icon name="arrow-right" color="FFF" size={24} />
+                            </Text>
+                        </View>
+                        <Text style={styles.buttonText}>
+                            Entrar
                     </Text>
-                </RectButton>
-            </View>
-        </ImageBackground>
+                    </RectButton>
+                </View>
+            </ImageBackground>
+        </KeyboardAvoidingView>
     );
 };
 
--- NextLevelWeek/mobile/src/pages/Points/index.tsx
+++ NextLevelWeek/mobile/src/pages/Points/index.tsx
@@ -1,5 +1,5 @@
 import { Feather as Icon } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import React, { useEffect, useState } from 'react';
@@ -24,6 +24,11 @@ interface Point {
     longitude: number;
 }
 
+interface Params {
+    uf: string;
+    city: string;
+}
+
 const Points = () => {
 
     const [items, setItems] = useState<Item[]>([]);
@@ -33,6 +38,9 @@ const Points = () => {
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as Params;
 
     useEffect(() => {
         async function loadPosition() {
@@ -65,14 +73,14 @@ const Points = () => {
     useEffect(() => {
         api.get('points', {
             params: {
-                city: 'Porto Alegre',
-                uf: 'RS',
-                items: [1, 2, 3, 4, 5, 6]
+                city: routeParams.city,
+                uf: routeParams.uf,
+                items: selectedItems
             }
         }).then(response => {
             setPoints(response.data);
         })
-    }, []);
+    }, [selectedItems]);
 
     function handleNavigateBack() {
         navigation.goBack();
